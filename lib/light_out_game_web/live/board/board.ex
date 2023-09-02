4597b0949dac986849f0442841a209fc54cf2035
Henrique Artur <contato@henriqueartur.com>
2023-09-02 19:51:55 -0300

    :sparkles: Find_adjacent_tiles/2

--- lib/light_out_game_web/live/board/board.ex
+++ lib/light_out_game_web/live/board/board.ex
@@ -11,7 +11,20 @@ defmodule LightOutGameWeb.Board do
     grid_x = String.to_integer(strX)
     grid_y = String.to_integer(strY)
 
-    update_grid = Map.put(grid, {grid_x, grid_y}, !grid[{grid_x, grid_y}])
+    update_grid = 
+      find_adjacent_tiles(grid_x, grid_y)
+      |> Enum.reduce(%{}, fn point, acc ->
+        Map.put(acc, point, !grid[point])
+      end)
+      |> then(fn toggle_grid -> Map.merge(grid, toggle_grid) end)
     {:noreply, assign(socket, :grid, update_grid)}
+  end
+
+  defp find_adjacent_tiles(x, y) do
+    prevX = Kernel.max(0, x - 1)
+    nextX = Kernel.min(4, x + 1)
+    prevY = Kernel.max(0, y - 1)
+    nextY = Kernel.min(4, y + 1)
+    [{x, y}, {prevX, y}, {nextX, y}, {x, prevY}, {x, nextY}]
   end
 end
